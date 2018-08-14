6838db5e17c2fc0ac7cb62aee187d3791710a74f
Lina Brunken <linatt@hotmail.de>
2018-08-14 11:57:15 +0200

    Passport vue.js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+  <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+          <div class="card-body">
+            @if (session('status'))
+              <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+              </div>
+            @endif
 
-                    You are logged in!
+            You are logged in!
 
-                    <a href="/sendrequest">Send Request</a>
-                </div>
+            <a href="/sendrequest">Send Request</a>
+
+            <div class="row">
+              <passport-clients></passport-clients>
+              <passport-authorized-clients></passport-authorized-clients>
+              <passport-personal-access-tokens></passport-personal-access-tokens>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
