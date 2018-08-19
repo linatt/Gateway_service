ca7d91c328bcc85bab6725eaf8b285f78ba9a081
Lina Brunken <linatt@hotmail.de>
2018-08-19 13:29:50 +0200

    jwt

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,20 +8,14 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                        @csrf
+                    <form method="POST" action="http://auth-service.homestead/jwtauth/login" aria-label="{{ __('Login') }}">
 
                         <div class="form-group row">
                             <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control" name="email" required autofocus>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
                             </div>
                         </div>
 
@@ -29,17 +23,13 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password" required>
 
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                    <!--    <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -49,7 +39,7 @@
                                     </label>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -14,6 +14,19 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+
+      $response = $this->call('GET', '/sendrequest');
+  //    $this->assertEquals(200, $response->status());
+//  $client = new Client();
+//  $result = $client->get('https://gateway.homestead/sendrequest')->getBody()->read(128);
+//  echo $response;
+        $A =$response[0]
+        echo $A;
+    //  $this->assertEquals('hallo', $response->getBody()->read(10));
+      //$this->assertTrue(true);
+      // $response2 = $this->call('GET', '/sendrequest');
+      // $this->assertEquals(200, $response2->status());
+      // $this->assertEquals(hilfe, $response2);
     }
+
 }
