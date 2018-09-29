aa2fa17776940a6dfd6ff3a11a315c22aeb42c28
Lina Brunken <linatt@hotmail.de>
2018-09-29 23:54:53 +0200

    tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -26,9 +26,22 @@ class ExampleTest extends TestCase
       //  echo $A;
     //  $this->assertEquals('hallo', $response->getBody()->read(10));
       //$this->assertTrue(true);
-       $response2 = $this->call('GET', '/sendrequest?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsdW1lbi1qd3QiLCJzdWIiOjEsImlhdCI6MTUzNTk3MzEyMSwiZXhwIjoxNTM1OTc2NzIxfQ.mH-Wjq8x3PoDawkrIq767ze4wQPtIegWG77XrDSQ0Oc');
-       $this->assertEquals(200, $response2->status());
-       $this->assertEquals('["[\"Antwort vom A-Service!\",\"Antwort vom B-Service!\"]"]', $response2->getContent());
+
+      //JWT
+      $response2 = $this->call('GET', '/sendrequest?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsdW1lbi1qd3QiLCJzdWIiOjEsImlhdCI6MTUzNTk3MzEyMSwiZXhwIjoxNTM1OTc2NzIxfQ.mH-Wjq8x3PoDawkrIq767ze4wQPtIegWG77XrDSQ0Oc');
+      $this->assertEquals(200, $response2->status());
+      $this->assertEquals('["[\"Antwort vom A-Service!\",\"Antwort vom B-Service!\"]"]', $response2->getContent());
+
+      $response2 = $this->call('GET', '/sendrequest?token=ungueltigertoken');
+      $this->assertEquals(400, $response2->status());
+
+      //PASETO
+      $response2 = $this->call('GET', '/sendpasetorequest?paseto_token=v2.local._IBZaBTnazcIADhMxzPgaNpSmeKgb7r9CNChAZTFwLUtfH-ArNdi3jRBbQLfRHAWV8zCuMaWfOVgvTOI8vFo9XZTwj7WFgzZCrCnWdZZw7yaRCEnrugznb23fWAhDA');
+      $this->assertEquals(200, $response2->status());
+      $this->assertEquals('["[\"Antwort vom A-Service!\",\"Antwort vom B-Service!\"]"]', $response2->getContent());
+
+      $response2 = $this->call('GET', '/sendpasetorequest?paseto_token=ungültigerToken');
+      $this->assertEquals(401, $response2->status());
     }
 
 }
